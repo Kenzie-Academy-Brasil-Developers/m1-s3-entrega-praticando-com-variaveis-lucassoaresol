624b22ed235f0ed6a1aabd8486048aeb587a559a
Lucas <lucassoares2007@hotmail.com>
2022-06-13 18:26:32 -0300

    atividade completa

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+let nome            = "Lucas"
+let sobrenome       = "Soares"
+let anoNascimento   = 1996
+let cidade          = "Santa Quitéria"
+let idadeAproximada = 2022 - anoNascimento
+
+console.log("Eu sou o",nome,sobrenome,"tenho",idadeAproximada,"anos e moro em",cidade)
