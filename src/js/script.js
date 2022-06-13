86a0de20fc881d30952092345a70300d9c6c0ab9
Lucas <lucassoares2007@hotmail.com>
2022-06-13 19:19:01 -0300

    atualizando sprint

--- src/js/script.js
+++ src/js/script.js
@@ -4,4 +4,4 @@ let anoNascimento   = 1996
 let cidade          = "Santa Quitéria"
 let idadeAproximada = 2022 - anoNascimento
 
-console.log("Eu sou o",nome,sobrenome,"tenho",idadeAproximada,"anos e moro em",cidade)
+console.log(`Eu sou o ${nome} ${sobrenome} tenho ${idadeAproximada} anos e moro em ${cidade}`)
